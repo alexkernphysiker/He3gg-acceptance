79a37554cbdcaff9eb16395149c3b8edf47e6e67
rundel <alexrndl@gmail.com>
2018-03-20 10:31:25 +0200

    cpp file added

---
+++ he3gg.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math_h/lorentzvector.h>
+#include <gnuplot_wrap.h>
+using namespace std;
+using namespace MathTemplates;
+using namespace GnuplotWrap;
+int main()
+{
+}
