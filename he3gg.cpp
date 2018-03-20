4de1a4725a3bb48e32814e07a66fab49320789ce
rundel <alexrndl@gmail.com>
2018-03-20 15:00:36 +0200

    he3gg(Q) acceptance plot

--- he3gg.cpp
+++ he3gg.cpp
@@ -9,18 +9,21 @@ using namespace MathTemplates;
 using namespace GnuplotWrap;
 int main()
 {
-    	Plotter::Instance().SetOutput(".","sim-2g");
+    	Plotter::Instance().SetOutput(".","simulation-geom");
 	const RandomValueTableDistr<>pf3=ReadPfFromFile("distributions/he3eta-pf-90-20.txt");
         const RandomUniform<> Pb_distr(p_beam_low,p_beam_hi); 
-        const size_t ev_count=100000;
-        size_t count=0;
+        const auto M_thr=Particle::he3().mass()+Particle::eta().mass();
+        const size_t ev_count=10000000;
+        Distribution1D<> registered_count(BinsByStep(-70.,2.5,30.));
+        Distribution1D<> all_count(BinsByStep(-70.,2.5,30.));
         for(size_t event=0;event<ev_count;event++){
             const auto C=Compound(Pb_distr,pf3);
+            const auto Q=((C.he3+C.eta_).M()-M_thr)*1000.;
             const auto gammas_cme=binaryDecay(C.eta_.M(),0.0,0.0,randomIsotropic<3>());
             const auto g1Plab=gammas_cme.first.Transform(-C.eta_.Beta());
             const auto g2Plab=gammas_cme.second.Transform(-C.eta_.Beta());
             const auto he3dir=direction(C.he3.P());
-            
+            all_count.Fill(Q);
             if((he3dir.th()>(3.0*PI()/180.))&&(he3dir.th()<(18.0*PI()/180.))){
                 const auto g1dir=direction(g1Plab.P());
                 const auto g2dir=direction(g2Plab.P());
@@ -28,9 +31,8 @@ int main()
                     (g1dir.th()>(20.*PI()/180.))&&(g1dir.th()<(169.*PI()/180.))&&
                     (g2dir.th()>(20.*PI()/180.))&&(g2dir.th()<(169.*PI()/180.))
                 )
-                    count++;
+                    registered_count.Fill(Q);
             }
         }
-        const auto acceptance=std_error(double(count))/ev_count;
-        cout<<"Geometry acceptance is "<<acceptance<<endl;
+        Plot().Hist(registered_count/all_count)<<"set yrange [0:]";
 }
